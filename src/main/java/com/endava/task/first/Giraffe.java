f275f33a7c126c9224ed9339a3ab1b2e9c194fc0
iturchina <turkina.ira@gmail.com>
2020-10-26 09:52:53 +0200

    created first task

---
+++ src/main/java/com/endava/task/first/Giraffe.java
@@ -0,0 +1,101 @@
+package com.endava.task.first;
+
+import java.time.LocalDateTime;
+
+public class Giraffe {
+
+    private String name;
+    private int kgOfFood;
+    private float height;
+    private double weight;
+    private char gender;
+    private LocalDateTime birthDate;
+    private boolean vipLevel;
+
+    public Giraffe(String name, int kgOfFood, float height, double weight, char gender, LocalDateTime birthDate, boolean vipLevel) {
+        this.name = name;
+        this.kgOfFood = kgOfFood;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.vipLevel = vipLevel;
+    }
+
+    public Giraffe(String name, char gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public Giraffe() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getKgOfFood() {
+        return kgOfFood;
+    }
+
+    public void setKgOfFood(int kgOfFood) {
+        this.kgOfFood = kgOfFood;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isVipLevel() {
+        return vipLevel;
+    }
+
+    public void setVipLevel(boolean vipLevel) {
+        this.vipLevel = vipLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "Giraffe{" +
+                "name='" + name + '\'' +
+                ", kgOfFood=" + kgOfFood +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", gender=" + gender +
+                ", birthDate=" + birthDate +
+                ", vipLevel=" + vipLevel +
+                '}';
+    }
+}
